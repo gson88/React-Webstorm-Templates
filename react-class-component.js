fd65f4f049ce757c707aac24ee8ea42a14357a2c
Johan <johan.v.gustafsson@husqvarnagroup.com>
2017-09-27 08:32:20 +0200

    Class component

---
+++ react-class-component.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ${NAME} extends Component{
+    constructor(props){
+        super(props);
+    }
+    
+    render(){
+        return (
+            <div/>
+        );
+    }
+}
+
+${NAME}.propTypes = {
+
+};
+
+export default ${NAME};
