58ccce563b5474824d077a7db694f5a75a1b8364
gson88 <gsonmx@gmail.com>
2018-02-07 11:08:10 +0100

    Update react-redux-reducer.js

--- react-redux-reducer.js
+++ react-redux-reducer.js
@@ -1,4 +1,4 @@
-import ${DATA_CLASS_NAME} from './data/${DATA_CLASS_NAME}.js'
+import ${DATA_CLASS_NAME} from '${DATA_FOLDER}/${DATA_CLASS_NAME}'
 
 function getInitialState() {
   return new ${DATA_CLASS_NAME}();
