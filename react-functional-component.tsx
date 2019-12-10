e90ac0f3a42179e2e255249acf2275024018b79e
gson88 <gsonmx@gmail.com>
2019-12-10 15:03:41 +0100

    Update and rename react-functional-component.js to react-functional-component.tsx

--- react-functional-component.tsx
+++ react-functional-component.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const ${NAME} = props => {
+interface Props {
+
+}
+
+const ${NAME}: React.FC<Props> = props => {
   return (
     <div className="${NAME}">
-
+    
     </div>
   );
-};
-
-${NAME}.propTypes = {
-
 };
 
 export default ${NAME};
